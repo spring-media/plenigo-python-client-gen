29abeaaddc9cf8bf733788cfc5b110f1d7e1f83e
Christian Hunter <christian.reynoso-hunter@axelspringer.com>
2024-10-18 11:07:14 +0200

    Update api_base_created_by_type.py
    Adds IMPORTER type

--- plenigo-client/plenigo/models/api_base_created_by_type.py
+++ plenigo-client/plenigo/models/api_base_created_by_type.py
@@ -5,6 +5,7 @@ class ApiBaseCreatedByType(str, Enum):
     API = "API"
     CUSTOMER = "CUSTOMER"
     MERCHANT = "MERCHANT"
+    IMPORTER = "IMPORTER"
     SYSTEM = "SYSTEM"
 
     def __str__(self) -> str:
