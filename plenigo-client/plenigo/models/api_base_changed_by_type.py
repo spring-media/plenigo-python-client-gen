70299ee065098c5439161b68b4dea2287109b81e
Christian Hunter <christian.reynoso-hunter@axelspringer.com>
2024-10-21 11:09:42 +0200

    Update api_base_changed_by_type.py
    Adds IMPORTER to ApiBaseChangedByType

--- plenigo-client/plenigo/models/api_base_changed_by_type.py
+++ plenigo-client/plenigo/models/api_base_changed_by_type.py
@@ -5,6 +5,7 @@ class ApiBaseChangedByType(str, Enum):
     API = "API"
     CUSTOMER = "CUSTOMER"
     MERCHANT = "MERCHANT"
+    IMPORTER = "IMPORTER"
     SYSTEM = "SYSTEM"
 
     def __str__(self) -> str:
